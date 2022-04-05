a1a7e844b3f99de781ae9aec94384851beb06195
john gallagher <127highstreet@gmail.com>
2022-04-05 14:36:01 -0400

    saving for now

---
+++ HeritageTree/Models/Ownership.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HeritageTree.Models
+{
+    public class Ownership
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
