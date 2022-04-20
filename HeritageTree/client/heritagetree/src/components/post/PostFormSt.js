2065a8f57593795ef19f9c0c95b65a25ac6d8c6b
john gallagher <127highstreet@gmail.com>
2022-04-19 23:17:17 -0400

    saving

--- HeritageTree/client/heritagetree/src/components/post/PostFormSt.js
+++ HeritageTree/client/heritagetree/src/components/post/PostFormSt.js
@@ -6,11 +6,12 @@ import {WardContext} from "../../providers/WardProvider";
 import { TreeCommonNameContext } from "../../providers/TreeCommonNameProvider";
 import { HeritageStatusContext } from "../../providers/HeritageStatusProvider";
 import { OwnershipContext } from "../../providers/OwnershipProvider";
-import { HealthStatusContext } from "../../providers/HealthStatusProvider"
+import { HealthStatusContext } from "../../providers/HealthStatusProvider";
+import { GeoCoordinateContext } from "../../providers/GeoCoordinateProvider";
 import { Button, Form, FormSelect} from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export const PostFormStMesage = "This page is under construction right now.  Please come back later."
+
 
 export const PostFormSt = () => {
     
@@ -24,18 +25,32 @@ export const PostFormSt = () => {
     const { heritageStatuses, getAllHeritageStatuses } = useContext(HeritageStatusContext);
     const { ownerships, getAllOwnerships } = useContext(OwnershipContext);
     const {healthStatuses, getAllHealthStatuses} = useContext(HealthStatusContext);
+    
 
   useEffect(() => {
-    getAllMaintenances();
-    getAllWards();
-    getAllTreeCommonNames();
-    getAllHeritageStatuses();
-    getAllOwnerships();
-    getAllHealthStatuses();
+    getAllMaintenances()
+    .then(getAllWards())
+    .then(getAllTreeCommonNames())
+    .then(getAllHeritageStatuses())
+    .then(getAllOwnerships())
+    .then(getAllHealthStatuses())
   }, []);
 
 
-console.log(wards, maintenances, treeCommonNames, heritageStatuses, ownerships, healthStatuses)
+// console.log(wards, maintenances, treeCommonNames, heritageStatuses, ownerships, healthStatuses)
+
+const [query, setQuery] = useState ({
+        streetAddress: "",
+        city:"",
+        state:"",
+        zip: "",
+        latitude: "",
+        longitude: ""
+      });
+
+    const [results, setResults] = useState({
+
+    });
 
     const [post, setPost] = useState({
         streetAddress: "",
@@ -46,11 +61,20 @@ console.log(wards, maintenances, treeCommonNames, heritageStatuses, ownerships, 
         longitude: "",
         wardId: "",
         userProfileId: currentUserId,
-        treeCommonName: "",
+        treeCommonNameId: "",
         imageLocation: "",
         ownershipId:4, 
-        isApproved: false
+        healthStatusId: ""
     });
+
+
+    const getGeoCoordinate = (streetAddress, city, state, zip) => {
+        debugger
+        return fetch(`https://nominatim.openstreetmap.org/?street=${streetAddress}&city=${city}&state=${state}&postalcode=${zip}&format=jsonv2`)
+        .then((res) => res.json())
+        .then(setResults);
+        debugger
+      }
 
     const navigate = useNavigate();
 
@@ -58,58 +82,121 @@ console.log(wards, maintenances, treeCommonNames, heritageStatuses, ownerships, 
         const newPost = { ...post }
         newPost[e.target.name] = e.target.value
         setPost(newPost)
+        
     }
+
+    const handleControlledInputChangeGeoCoordinates = (e) => {
+      const newQuery = { ...query }
+      newQuery[e.target.name] = e.target.value
+      setQuery(newQuery)
+  }
 
     const handleClickSavePost = (e) => {
         e.preventDefault();
-        console.log(post);
-        addPost(post)
+        getGeoCoordinate(query.streetAddress, query.city, query.state, query.zip)
+        
+        .then( addPost(post))
         .then(() => navigate('/posts'));  
         
     }
 
     return (
         <>
-        <h3 className="postForm__title">Report Heritage Tree Form </h3>
-        <p> This form requires geographic coordinates of a tree, such as latitude and longitude.  If you would like to report with a street address, click the button below</p>
-        <Link to={`/posts/st/create`}>
-          <Button className="post__create">Report a Heritage Tree with a street address</Button>
+        <h3 className="postForm__title">Nominate a Heritage Tree with Street Address </h3>
+        <p> This form requires a street address of a tree.  If you would like to report with geographic coordinates, click the button below</p>
+        <Link to={`/posts/cor/create`}>
+          <Button className="post__create">Report a Heritage Tree with geographic coordinates</Button>
         </Link>
 
         <Form className="post__form">
-          <Form.Group className="mb-3" controlId="title">
-            <Form.Label>Title:</Form.Label>
-              <Form.Control name="title" value={post.title} onChange={handleControlledInputChange} type="text" required autoFocus placeholder="Enter a title for your post" />
-        </Form.Group>
-
-  <Form.Group className="mb-3" controlId="categoryId">
-    <Form.Label>Category:</Form.Label>
-    <FormSelect required autoFocus name="categoryId" onChange={handleControlledInputChange}>
-        <option>Select a category</option>
-        {maintenances.map((category) => {
+        <Form.Group className="mb-3" controlId="treeCommonNameId">
+            <Form.Label>Please select a type of the tree:</Form.Label>
+              <FormSelect required autoFocus name="treeCommonNameId" onChange={handleControlledInputChange}>
+        <option>Select a type of the tree</option>
+        {treeCommonNames.map((t) => {
             return (
-                <option value={category.id}>{category.name}</option>
+                <option key={t.id} value={t.id}>{t.name}</option>
             )
           })
         }
-    </FormSelect>
+              </FormSelect>
+          </Form.Group>
+
+  <Form.Group className="mb-3" controlId="streetAddress">
+    <Form.Label>Street Address:</Form.Label>
+    <Form.Control required autoFocus name="streetAddress" onChange={handleControlledInputChangeGeoCoordinates}/>
   </Form.Group>
 
-  <Form.Group className="mb-3" controlId="content">
-    <Form.Label>Content:</Form.Label>
-    <Form.Control name="content" value={post.content} onChange={handleControlledInputChange} type="text" required autoFocus placeholder="Enter content for your post" as="textarea" aria-label="With textarea" />
+  <Form.Group className="mb-3" controlId="city">
+    <Form.Label>City:</Form.Label>
+    <Form.Control required autoFocus name="city" onChange={handleControlledInputChangeGeoCoordinates}/>
   </Form.Group>
 
-  <Form.Group className="mb-3" controlId="imageLocation">
-    <Form.Label>Gif Image:</Form.Label>
-    <Form.Control name="imageLocation" value={post.imageLocation} onChange={handleControlledInputChange} type="text" autoFocus placeholder="Enter enter a url to your gif" />
+
+  <Form.Group className="mb-3" controlId="state">
+    <Form.Label>State:</Form.Label>
+    <Form.Control required autoFocus name="state" onChange={handleControlledInputChangeGeoCoordinates}/>
   </Form.Group>
 
-  <Form.Group className="mb-3" controlId="publishDateTime">
-    <Form.Label>Publish Date:</Form.Label>
-    <Form.Control name="publishDateTime" value={post.publishDateTime} onChange={handleControlledInputChange} type="date" autoFocus />
+  <Form.Group className="mb-3" controlId="zip">
+    <Form.Label>Zip:</Form.Label>
+    <Form.Control required autoFocus name="zip" onChange={handleControlledInputChangeGeoCoordinates}/>
   </Form.Group>
 
+  <Form.Group className="mb-3" controlId="wardId">
+            <Form.Label>Please select a ward:</Form.Label>
+              <FormSelect required autoFocus name="wardId" onChange={handleControlledInputChange}>
+        <option >Select a ward</option>
+        {wards.map((ward) => {
+            return (
+                <option key={ward.id} value={ward.id}>{ward.name}</option>
+            )
+          })
+        }
+              </FormSelect>
+              <Form.Text>If you do not know which ward, please visit <a href="https://coewv.maps.arcgis.com/apps/webappviewer/index.html?id=15246ac759c345babbbbb8a5f7c11490" target="_blank" rel="noopener noreferrer">here</a></Form.Text>
+          </Form.Group>
+
+          {/* <Form.Group className="mb-3" controlId="latitude">
+            <Form.Label>Latitude:</Form.Label>
+              <Form.Control name="latitude" value={post.latitude} onChange={handleControlledInputChange} type="text" required autoFocus placeholder="Enter a latitude" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="longitude">
+            <Form.Label>Longitude:</Form.Label>
+              <Form.Control name="longitude" value={post.longitude} onChange={handleControlledInputChange} type="text" required autoFocus placeholder="Enter a longitude" />
+          </Form.Group> */}
+
+<Form.Group className="mb-3" controlId="imageLocation">
+    <Form.Label>Image URL(optional):</Form.Label>
+    <Form.Control name="imageLocation" value={post.imageLocation} onChange={handleControlledInputChange} type="url" autoFocus placeholder="Enter a url of an image" />
+  </Form.Group>
+
+          <Form.Group className="mb-3" controlId="ownershipId">
+            <Form.Label>Please select a type of property the tree is located:</Form.Label>
+              <FormSelect  autoFocus name="ownershipId" onChange={handleControlledInputChange}>
+        <option >Select a type of property</option>
+        {ownerships.map((o) => {
+            return (
+                <option selected={post.ownershipId} key={o.id} value={o.id}>{o.name}</option>
+            )
+          })
+        }
+              </FormSelect>
+              </Form.Group>
+
+  <Form.Group className="mb-3" controlId="healthStatusId">
+            <Form.Label>Please select a health status of the tree:</Form.Label>
+              <FormSelect required autoFocus name="healthStatusId" onChange={handleControlledInputChange}>
+        <option>Select a health status of the tree</option>
+        {healthStatuses.map((h) => {
+            return (
+                <option key={h.id} value={h.id}>{h.name}</option>
+            )
+          })
+        }
+              </FormSelect>
+          </Form.Group>
 
   <Button onClick={handleClickSavePost} variant="primary" size="md" type="submit">
     Save Post
---
+++ HeritageTree/client/heritagetree/src/providers/GeoCoordinateProvider.js
@@ -0,0 +1,23 @@
+import React, { useState, createContext } from "react";
+
+export const GeoCoordinateContext = createContext();
+
+export const PostProvider = (props) => {
+    const [queries, setQueries] = useState([]);
+    const [results, setResults] = useState([]);
+
+
+    const getGeoCoordinate = (streetAddress, city, state, zip) => {
+        debugger
+        return fetch(`https://nominatim.openstreetmap.org/?street=${streetAddress}&city=${city}&state=${state}&postalcode=${zip}&format=jsonv2`)
+        .then((res) => res.json())
+        .then(setResults);
+        
+      }
+
+    return (
+        <GeoCoordinateContext.Provider value={{  }}>
+          {props.children}
+        </GeoCoordinateContext.Provider>
+      );
+    };
